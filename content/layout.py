6d344414f8d6f415fee879ef909d04b46dc97e77
Excidion <36995046+Excidion@users.noreply.github.com>
2022-08-01 17:39:45 +0200

    download plots

--- content/layout.py
+++ content/layout.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from time import sleep
+from content.model_utils import get_session_data
 
 class BasePage:
     def __init__(self, title, depends={"model"}):
@@ -8,15 +9,26 @@ class BasePage:
         self.build_page()
     
     def build_page(self):
+        st.session_state["img"] = None # reset image cache
+        # check all required files have been uploaded
         none_missing = True
         for i in self.depends:
             if not i in st.session_state.keys():
                 none_missing = False
                 st.warning(f"{self.title} requires {self.depends}. Please upload missing {i} in the 'Settings' page.")
-
+        # if all required uploads are there, display the page content
         if none_missing:
             st.title(self.title)
             self.get_content()
+            # if there has been an image generated and cached offer download
+            img = get_session_data("img")
+            st.download_button(
+                label = "Download Plot",
+                data = img if img is not None else "",
+                file_name = self.title.replace(" ", "-") + ".png",
+                mime = "image/png",
+                disabled = img is None,
+            )
 
     def get_content(self): # this methods needs to be filled for the actual pages
         sleep(3)
--- content/plot_utils.py
+++ content/plot_utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from matplotlib import pyplot as plt
 import mplcyberpunk
+from io import BytesIO
 
 
 def activate_neon_plots():
@@ -8,11 +9,17 @@ def activate_neon_plots():
 
 def display_figure():
     mplcyberpunk.make_lines_glow()
+    # save to buffer early to avoid transparent background
     fig = plt.gcf()
+    img = BytesIO()
+    plt.savefig(img, format="png")
+    st.session_state["img"] = img
+    # make background and canvas transparent
     fig.patch.set_facecolor('b')
     fig.patch.set_alpha(0)
     ax = plt.gca()
     ax.patch.set_facecolor('b')
     ax.patch.set_alpha(0)
+    # display and close
     st.pyplot(fig)
     plt.close()
