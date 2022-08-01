8f269c2ea63948b5944b894dca3c3c9219f3a727
Excidion <36995046+Excidion@users.noreply.github.com>
2022-08-01 09:36:17 +0200

    transparent figure and axes background and more subtle glow

--- content/plot_utils.py
+++ content/plot_utils.py
@@ -7,6 +7,12 @@ def activate_neon_plots():
     plt.style.use("cyberpunk")
 
 def display_figure():
-    mplcyberpunk.add_glow_effects()
-    st.pyplot(plt.gcf())
+    mplcyberpunk.make_lines_glow()
+    fig = plt.gcf()
+    fig.patch.set_facecolor('b')
+    fig.patch.set_alpha(0)
+    ax = plt.gca()
+    ax.patch.set_facecolor('b')
+    ax.patch.set_alpha(0)
+    st.pyplot(fig)
     plt.close()
